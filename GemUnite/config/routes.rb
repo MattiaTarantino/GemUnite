17e6fe19d90c78bcd15a8fbeda2c81c83d3e457b
RiccardoEbene <riccardo.ebene02@gmail.com>
2023-07-25 18:40:53 +0200

    crud actions sui progetti

--- GemUnite/config/routes.rb
+++ GemUnite/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :progettos
   devise_for :admin_users, ActiveAdmin::Devise.config
-  resources: utentes
-  root :to => redirect('/utentes')
+  root :to => redirect('/progettos')
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
