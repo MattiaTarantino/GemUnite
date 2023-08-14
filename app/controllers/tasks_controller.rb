f0488eb843c3d885f7a1a080066f765d73f7bf0f
paolocupini <cupini.1941511@studenti.uniroma1.it>
2023-08-14 11:33:16 +0200

    terminato new-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,10 +22,15 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
+   
+    checkpoint = Checkpoint.find(params[:checkpoint_id])
+    @task.checkpoint = checkpoint
+
+
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html { redirect_to project_checkpoint_path(project_id: params[:project_id], id: params[:checkpoint_id]), notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -67,6 +72,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:nome, :descrizione, :completato)
+      params.require(:task).permit(:nome, :descrizione, :completato, :checkpoint_id)
     end
 end
