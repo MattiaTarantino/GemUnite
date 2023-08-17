46dd60cd2b816dd97c1d8fbc26314bb8f27e7ac1
JacopoDellepiane <jacopodellepiane@icloud.com>
2023-08-18 00:24:16 +0200

    spostamento restrizione visualizzazione richieste a quelle aperte nel controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -19,7 +19,7 @@ class RequestsController < ApplicationController
   # GET /requests or /requests.json
   def index
     if @user_project.role == "leader"
-      @requests = @project.requests
+      @requests = @project.requests.where(stato_accettazione: "aperta")
     else
       redirect_to root_path
       flash[:notice] = "Solo il leader del progetto può accedere a questa pagina"
