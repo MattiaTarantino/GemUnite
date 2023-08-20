189f593b04a33af8fd4ae6e1ef5144f90f3320c5
Mattia <105870021+MattiaTarantino@users.noreply.github.com>
2023-08-20 12:34:21 +0200

    richieste di partecpiazione
    ora è possibile mandare richieste di partecipare, visualizzare quelle accettate, in attesa o rifiutate e eliminare quelle in attesa

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,27 +5,35 @@ class RequestsController < ApplicationController
   def index
     @requests = Request.all
   end
-
   # GET /requests/1 or /requests/1.json
   def show
+  end
+
+  def my_requests
+    @requests = Request.where(user_id: current_user.id)
   end
 
   # GET /requests/new
   def new
     @request = Request.new
+    @project = Project.where(id: params[:project_id]).first
   end
 
   # GET /requests/1/edit
   def edit
+    @request = Request.where(id: params[:id]).first
   end
 
   # POST /requests or /requests.json
   def create
     @request = Request.new(request_params)
+    @request.project_id = params[:project_id]
+    @request.user_id = current_user.id
+    @request.stato_accettazione = "In attesa"
 
     respond_to do |format|
       if @request.save
-        format.html { redirect_to request_url(@request), notice: "Request was successfully created." }
+        format.html { redirect_to project_requests_url, notice: "Request was successfully created." }
         format.json { render :show, status: :created, location: @request }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -52,7 +60,7 @@ class RequestsController < ApplicationController
     @request.destroy
 
     respond_to do |format|
-      format.html { redirect_to requests_url, notice: "Request was successfully destroyed." }
+      format.html { redirect_to my_requests_requests_path, notice: "Request was successfully destroyed." }
       format.json { head :no_content }
     end
   end
