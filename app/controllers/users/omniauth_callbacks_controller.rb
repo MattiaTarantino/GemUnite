f751bb4e89a18f123015fae6faadf8dd01fb6bf3
paolocupini <cupini.1941511@studenti.uniroma1.it>
2023-08-22 10:13:42 +0200

    redirect dopo sign in con github
    deve essere fixato update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      redirect_to edit_user_registration_path(@user) and return 
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url and return
     end
   end
 
