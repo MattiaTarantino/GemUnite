3d14d47952c9b32cde319c3f7a9762e40237e9ec
paolocupini <cupini.1941511@studenti.uniroma1.it>
2023-08-23 15:12:18 +0200

    github redirect
    still having issues with Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :current_admin_user
+  def after_sign_in_path_for(resource)
+
+  if resource.is_a?(User) && resource.provider == "github"
+    edit_user_registration_path(resource)
+  else
+    super
+  end
+end
 end
