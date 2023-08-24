619fb2cb2a49453421242b8c8f32090a996db468
RiccardoEbene <riccardo.ebene02@gmail.com>
2023-08-24 15:11:41 +0200

    bug fixes + edit page per oauth da fare

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :chats
   has_many :projects, through: :user_projects
 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
