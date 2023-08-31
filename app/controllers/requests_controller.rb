60538470a8132ac9c319ab087686419c9bfb9c95
JacopoDellepiane <jacopodellepiane@icloud.com>
2023-08-31 12:37:17 +0200

    layout request show e separazione partial per seguire linee guida owasp

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -36,7 +36,6 @@ class RequestsController < ApplicationController
   def index
     if @user_project && @user_project.role == "leader"
       @requests = @project.requests.where(stato_accettazione: "In attesa")
-      @owner = false
     else
       redirect_to root_path
       flash[:notice] = "Solo il leader del progetto può accedere a questa pagina"
@@ -45,10 +44,6 @@ class RequestsController < ApplicationController
 
   # GET /requests/1 or /requests/1.json
   def show
-    @request = Request.find(params[:id])
-    if @user.id == @request.user_id
-      @owner = true
-    end
   end
 
   def my_requests
