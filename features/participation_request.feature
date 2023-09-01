c72fd90ae0b7973aa2ce08783889d327abf6de0c
MattiaTarantino <mtarantino44@gmail.com>
2023-09-01 17:05:12 +0200

    acceptance us #14

--- features/participation_request.feature
+++ features/participation_request.feature
@@ -2,11 +2,7 @@ Feature: Requesting Participation in a Project
 
   Scenario: A registered user sends a participation request to a project
     Given I am a registered user
+    And i am logged in
     And there exists a project
-    When I click the "Partecipa" button on the project page
+    When I click the "Invia richiesta" button on the project request page
     Then I should see a confirmation message
-
-  Scenario: A registered user sends twice a participation request to a project
-    Given I am a registered user
-    When I click the "Partecipa" button on the project page on which the request had already been sent
-    Then I should see an error message
--- features/step_definitions/participation_request_steps.rb
+++ features/step_definitions/participation_request_steps.rb
@@ -1,24 +1,32 @@
 Given("I am a registered user") do
   # Implement code to create a registered user or use a testing user
-  User.create(username: "testuser", email: "test@example.com", password: "password")
+  @user = User.create(id:20, username: "testuser", email: "test@example.com", password: "Ciao1234!")
 end
 
 Given("there exists a project") do
   # Implement code to create a project or use an existing project
+  @project = Project.create(info_leader: "Leader info", dimensione: 5, descrizione: "Project description", stato: "aperto", name: "Project Name")
 end
 
-When("I click the {string} button on the project page") do |button_text|
-  # Implement code to simulate clicking the button and sending the request
+Given("i am logged in") do
+  visit new_user_session_path
+  fill_in 'Email', with: @user.email
+  fill_in 'Password', with: @user.password
+  click_button 'Log in'
+
+  expect(page).to have_content('Signed in successfully')
 end
 
-When("I click the {string} button on the project page on which the request had already been sent") do |button_text|
+When("I click the {string} button on the project request page") do |button_text|
   # Implement code to simulate clicking the button and sending the request
+  visit new_user_project_request_path(user_id: @user.id, project_id: @project.id)
+
+  fill_in 'Note', with: 'Vorrei partecipare a questo progetto'
+
+  click_button button_text
 end
 
 Then("I should see a confirmation message") do
   # Implement code to check for the confirmation message
-end
-
-Then("I should see an error message") do
-  # Implement code to check for the error message
+  expect(page).to have_content('Richiesta effettuata')
 end
