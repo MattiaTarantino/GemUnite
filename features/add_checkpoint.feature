94bceb7cf3a3cfa3b55323ee2dd63b428d89114b
RiccardoEbene <riccardo.ebene02@gmail.com>
2023-09-01 16:05:45 +0200

    acceptance test per user story aggiunta checkpoint

--- features/add_checkpoint.feature
+++ features/add_checkpoint.feature
@@ -1,11 +1,12 @@
 Feature: Adding a checkpoint
 
-  Scenario: User adds a checkpoint through a button
-    Given the user is logged in
-    And the user is on the project page
-    And the user is the leader of the project
-    When the user clicks on the "Aggiungi Checkpoint" button
-    And fills in the checkpoint details
-    And submits the form
-    Then the user should see a success message
-    And the user should be redirected to the checkpoint page
+  Scenario: Leader adds a checkpoint through a button
+    Given I am registered as user
+    And I am the leader of a started project
+    And I am logged in
+    And I am on the project page
+    When I click the "Aggiungi checkpoint" button
+    And I fill in the checkpoint details
+    And I submit the form
+    Then I should see a success message
+    And I should be redirected to the checkpoint page
--- features/step_definitions/add_checkpoint_steps.rb
+++ features/step_definitions/add_checkpoint_steps.rb
@@ -1,31 +1,53 @@
+# Import necessary libraries and set up your testing environment as needed.
 
-Given("the user is logged in") do
+Given("I am registered as user") do
+  @user = User.create(id: 12, username: "testuser", email: "test@user.com", password: "Ciao1234!")
 end
 
-And("the user is on the project page") do
-  # Implement code to visit the project page
+Given("I am the leader of a started project") do
+  @project = Project.create(name: "testproject", dimensione: 3, info_leader: "test info", stato: "iniziato", descrizione: "testdescription")
+  @chat = Chat.create(project_id: @project.id)
+  user_project = UserProject.create(user_id: @user.id, project_id: @project.id, role: "leader")
 end
 
-And("the user is the leader of the project") do
-  # Implement code to verify user's leadership status
+Given("I am logged in") do
+  visit new_user_session_path
+  fill_in 'Email', with: 'test@user.com'
+  fill_in 'Password', with: 'Ciao1234!'
+  click_button 'Accedi'
+
+  expect(page).to have_content('Signed in successfully')
 end
 
-When("the user clicks on the {string} button") do |button_text|
-  # Implement code to simulate clicking the button
+Given("I am on the project page") do
+  visit user_project_show_my_project_path(user_id: @user.id, project_id: @project.id)
 end
 
-And("fills in the checkpoint details") do
-  # Implement code to fill in checkpoint details in the form
+
+When("I click the {string} button") do |button_text|
+  click_link button_text
 end
 
-And("submits the form") do
-  # Implement code to submit the form
+When("I fill in the checkpoint details") do
+  fill_in 'Nome', with: 'testcheckpoint'
+  fill_in 'Descrizione', with: 'chekpoint testdescription'
 end
 
-Then("the user should see a success message") do
-  # Implement code to check for the success message on the page
+When("I submit the form") do
+  click_button "Create Checkpoint"
 end
 
-And("the user should be redirected to the checkpoint page") do
-  # Implement code to check for the correct redirection
+Then("I should see a success message") do
+  expect(page).to have_content('Checkpoint creato con successo.')
 end
+
+Then("I should be redirected to the checkpoint page") do
+  # Capture the ID of the newly created checkpoint from the URL
+  checkpoint_id = current_url.split('/').last
+
+  # Construct the expected path using the captured checkpoint ID
+  expected_path = user_project_checkpoint_path(user_id: @user.id, project_id: @project.id, id: checkpoint_id)
+
+  expect(page).to have_current_path(expected_path)
+end
+
